3cd3d0f2b87240ec9468c2eb7b414b4350da4a13
Aram Simonian <aram.contacts@gmail.com>
2017-11-29 16:39:03 +0000

    Created case statement for first part of Aged Brie logic

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -9,27 +9,34 @@ class GildedRose
 
   def self.update_quality()
     @items.each do |item|
-      if item.name == "Aged Brie" or item.name == "Backstage passes to a TAFKAL80ETC concert"
+      case item.name
+      when "Aged Brie"
         if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
+          item.quality += 1
+        end
+      else
+        if item.name == "Backstage passes to a TAFKAL80ETC concert"
+          if item.quality < 50
+            item.quality = item.quality + 1
+            if item.name == "Backstage passes to a TAFKAL80ETC concert"
+              if item.sell_in < 11
+                if item.quality < 50
+                  item.quality = item.quality + 1
+                end
               end
-            end
 
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
+              if item.sell_in < 6
+                if item.quality < 50
+                  item.quality = item.quality + 1
+                end
               end
             end
           end
-        end
-      else
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
+        else
+          if item.quality > 0
+            if item.name != "Sulfuras, Hand of Ragnaros"
+              item.quality = item.quality - 1
+            end
           end
         end
       end
