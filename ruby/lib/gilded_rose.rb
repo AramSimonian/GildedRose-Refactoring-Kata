6db5f67a86974066d5e07abd5efb652d7173e808
Aram Simonian <aram.contacts@gmail.com>
2017-11-29 17:05:07 +0000

    Created case for Sulfuras and removed from main code body

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -20,6 +20,8 @@ class GildedRose
         if item.sell_in < 0 && item.quality < 50
           item.quality += 1
         end
+      when "Sulfuras, Hand of Ragnaros"
+        # do nothing
       else
         if item.name == "Backstage passes to a TAFKAL80ETC concert"
           if item.quality < 50
@@ -36,24 +38,16 @@ class GildedRose
                 item.quality = item.quality + 1
               end
             end
-
           end
         else
           if item.quality > 0
-            if item.name != "Sulfuras, Hand of Ragnaros"
-              item.quality = item.quality - 1
-            end
+              item.quality -= 1
           end
         end
 
-        if item.name != "Sulfuras, Hand of Ragnaros"
-          item.sell_in = item.sell_in - 1
-        end
+        item.sell_in -= 1
 
-      end
-
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
+        if item.sell_in < 0
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
               if item.name != "Sulfuras, Hand of Ragnaros"
@@ -64,7 +58,9 @@ class GildedRose
             item.quality = item.quality - item.quality
           end
         end
+
       end
+
     end
   end
 end
