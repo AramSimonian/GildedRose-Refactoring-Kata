5ddcd75df056ec7a21810a6018619d759d7f2a2d
Aram Simonian <aram.contacts@gmail.com>
2017-11-29 16:52:06 +0000

    Cleaning up IFs for backstage passes

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -18,19 +18,19 @@ class GildedRose
         if item.name == "Backstage passes to a TAFKAL80ETC concert"
           if item.quality < 50
             item.quality = item.quality + 1
-            if item.name == "Backstage passes to a TAFKAL80ETC concert"
-              if item.sell_in < 11
-                if item.quality < 50
-                  item.quality = item.quality + 1
-                end
-              end
 
-              if item.sell_in < 6
-                if item.quality < 50
-                  item.quality = item.quality + 1
-                end
+            if item.sell_in < 11
+              if item.quality < 50
+                item.quality = item.quality + 1
               end
             end
+
+            if item.sell_in < 6
+              if item.quality < 50
+                item.quality = item.quality + 1
+              end
+            end
+
           end
         else
           if item.quality > 0
